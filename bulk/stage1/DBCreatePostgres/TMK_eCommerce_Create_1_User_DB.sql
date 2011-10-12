8483759d05614911ecb146ac55a467b68b4f6215
Jose Luis Campanello <jose.luis.campanello@gmail.com>
2011-10-12 10:38:41 -0300

    Fix db creation locale from es_AR.utf8 to es_AR.UTF-8

--- bulk/stage1/DBCreatePostgres/TMK_eCommerce_Create_1_User_DB.sql
+++ bulk/stage1/DBCreatePostgres/TMK_eCommerce_Create_1_User_DB.sql
@@ -11,6 +11,6 @@ CREATE DATABASE eCommerce
     WITH    OWNER = eCommerce
             TEMPLATE = template0
             ENCODING = 'UTF-8'
-            LC_COLLATE = 'es_AR.utf8'
-            LC_CTYPE = 'es_AR.utf8';
+            LC_COLLATE = 'es_AR.UTF-8'
+            LC_CTYPE = 'es_AR.UTF-8';
 
