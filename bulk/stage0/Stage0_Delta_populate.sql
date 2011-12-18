236d2b22fc6ae40f798de1ecc72cd16e803c868b
Jose Luis Campanello <jose.luis.campanello@gmail.com>
2011-12-18 20:07:55 -0300

    Added missing entries to the Delta table (IMPRints, CONTributors and other)

--- bulk/stage0/Stage0_Delta_populate.sql
+++ bulk/stage0/Stage0_Delta_populate.sql
@@ -42,3 +42,58 @@ SELECT          'PROD'                  AS EntityType,
     ORDER BY    Id_Articulo;
 
 COMMIT;
+
+--
+-- IMPR - Editores (imprints)
+--
+INSERT INTO Stage0_Delta(EntityType, EntityId, FlagUpdated, FlagMain,
+                        FlagPrice, FlagComments, FlagRelated, LastUpdate)
+SELECT          'IMPR'                  AS EntityType,
+                Id_Editor               AS EntityId,
+                0                       AS FlagUpdated,
+                0                       AS FlagMain,
+                0                       AS FlagPrice,
+                0                       AS FlagComments,
+                0                       AS FlagRelated,
+                CURRENT_TIMESTAMP       AS LastUpdate
+    FROM        Editores
+    ORDER BY    Id_Editor;
+
+COMMIT;
+
+--
+-- CONT - Autores (contributors)
+--
+INSERT INTO Stage0_Delta(EntityType, EntityId, FlagUpdated, FlagMain,
+                        FlagPrice, FlagComments, FlagRelated, LastUpdate)
+SELECT          'CONT'                  AS EntityType,
+                Id_Autor                AS EntityId,
+                0                       AS FlagUpdated,
+                0                       AS FlagMain,
+                0                       AS FlagPrice,
+                0                       AS FlagComments,
+                0                       AS FlagRelated,
+                CURRENT_TIMESTAMP       AS LastUpdate
+    FROM        Autores
+    ORDER BY    Id_Autor;
+
+COMMIT;
+
+--
+-- _DSP - Disponibilidades (marcas basicas)
+--
+INSERT INTO Stage0_Delta(EntityType, EntityId, FlagUpdated, FlagMain,
+                        FlagPrice, FlagComments, FlagRelated, LastUpdate)
+SELECT          '_DSP'                  AS EntityType,
+                Id_Disponibilidad       AS EntityId,
+                0                       AS FlagUpdated,
+                0                       AS FlagMain,
+                0                       AS FlagPrice,
+                0                       AS FlagComments,
+                0                       AS FlagRelated,
+                CURRENT_TIMESTAMP       AS LastUpdate
+    FROM        Disponibilidades
+    WHERE       Id_Esquema = 'PROD'
+    ORDER BY    Id_Disponibilidad;
+
+COMMIT;
